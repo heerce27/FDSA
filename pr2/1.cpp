a83243428fdd642063ed053eef1d3a6f82a3bf60
H27 <heershah.ce@gmail.com>
2026-07-22 00:30:37 +0530

    Added Practical 2

--- pr2/1.cpp
+++ pr2/1.cpp
@@ -1,6 +1,55 @@
 #include<iostream>
 using namespace std;
+int recursiveSearch(string arr[], int n, string s, int index)
+{
+    if (index >= n)
+    {
+        return -1; 
+    }
+    if (arr[index] == s)
+    {
+        return index; 
+    }
+    return recursiveSearch(arr, n, s, index + 1); 
+}
 int main()
 {
-
+    cout<<"Enter no of vehicles : ";
+    int n;
+    cin>>n;
+    string arr[n];
+    for (int i=0;i<n;i++)
+    {
+        cout<<"Enter licanse plate number : ";
+        cin>>arr[i];
+    }
+    cout<<"Enter licanse plate number to search : ";
+    string s;
+    cin>>s;
+    int f=0;
+    for (int i=0;i<n;i++)
+    {
+        if (arr[i]==s)
+        {
+            cout<<"Vehicle found at index : "<<i+1;
+            f=1;
+            break;
+        }
+    }
+    if (f==0)
+    {
+        cout<<"Vehicle not found";
+    }
+    cout<<"\nUsing recursive search : ";
+    int index = recursiveSearch(arr, n, s, 0);
+    if (index != -1)
+    {
+        cout<<"Vehicle found at index : "<<index+1;
+    }
+    else
+    {
+        cout<<"Vehicle not found";
+    }
+    return 0;
+    
 }
---
+++ pr2/2.cpp
@@ -0,0 +1,57 @@
+#include<iostream>
+using namespace std;
+int recursiveSearch(int arr[], int start, int end, int target)
+{
+    if(start>end)
+    return -1;
+    int mid=(start+end)/2;
+    if(arr[mid]==target)
+    return mid;
+    else if(arr[mid]<target)
+    {
+        return recursiveSearch(arr,mid+1,end,target);
+    }
+    else{
+        return recursiveSearch(arr,start,mid-1,target);
+    }
+}
+int main()
+{
+    cout<<"Enter no of codes:";
+    int n;
+    cin>>n;
+    int arr[n];
+    int s=0;
+    for(int i=0;i<n;i++)
+    {
+        arr[i]=s++;
+    }
+    int f;
+    cout<<"Enter code to search : ";
+    cin>>f;
+    int start=0,end=n-1;
+    while(start<=end)
+    {
+        int mid=(start+end)/2;
+        if(arr[mid]==f)
+        {
+            cout<<"Code found at index : "<<mid+1;
+            break;
+        }
+        else if(arr[mid]<f)
+        {
+            start=mid+1;
+        }
+        else
+        {
+            end=mid-1;
+        }
+    }
+
+    int r=recursiveSearch(arr,0,n-1,f);
+    if(r!=-1)
+    cout<<"Code found at "<<r+1<<" position";
+    else
+    cout<<"Code not found";
+    
+}
